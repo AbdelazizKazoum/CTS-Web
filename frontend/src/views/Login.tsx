012e075793b16a5fb6da0e2b59c31af7ce59f098
Abdelaziz_kazoum <abdukazoum@gmail.com>
2024-09-04 11:18:16 +0100

    create a new login form

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -1,209 +1,108 @@
-/* eslint-disable padding-line-between-statements */
+/* eslint-disable import/no-unresolved */
 'use client'
 
 // React Imports
-import { useEffect, useState } from 'react'
-
-// Next Imports
-import { redirect, useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 // MUI Imports
-import useMediaQuery from '@mui/material/useMediaQuery'
-import { styled, useTheme } from '@mui/material/styles'
-import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
-import InputAdornment from '@mui/material/InputAdornment'
-import Checkbox from '@mui/material/Checkbox'
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import Checkbox from '@mui/material/Checkbox'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
 import FormControlLabel from '@mui/material/FormControlLabel'
-import Divider from '@mui/material/Divider'
+import InputAdornment from '@mui/material/InputAdornment'
+import IconButton from '@mui/material/IconButton'
 
-// Third-party Imports
-import classnames from 'classnames'
-
-// Type Imports
-import type { SystemMode } from '@core/types'
-
-// Component Imports
-import Link from '@components/Link'
-import Logo from '@components/layout/shared/Logo'
+// Components Imports
 import CustomTextField from '@core/components/mui/TextField'
 
-// Config Imports
-import themeConfig from '@configs/themeConfig'
+import Logo from '@/components/layout/shared/Logo'
+import { FormHelperText } from '@mui/material'
 
-// Hook Imports
-import { useImageVariant } from '@core/hooks/useImageVariant'
-import { useSettings } from '@core/hooks/useSettings'
-import { UseAuthStore } from '@/store/auth.store'
-
-// Styled Custom Components
-const LoginIllustration = styled('img')(({ theme }) => ({
-  zIndex: 2,
-  blockSize: 'auto',
-  maxBlockSize: 680,
-  maxInlineSize: '100%',
-  margin: theme.spacing(12),
-  [theme.breakpoints.down(1536)]: {
-    maxBlockSize: 550
-  },
-  [theme.breakpoints.down('lg')]: {
-    maxBlockSize: 450
-  }
-}))
-
-const MaskImg = styled('img')({
-  blockSize: 'auto',
-  maxBlockSize: 355,
-  inlineSize: '100%',
-  position: 'absolute',
-  insetBlockEnd: 0,
-  zIndex: -1
-})
-
-const LoginV2 = ({ mode }: { mode: SystemMode }) => {
+const FormLayoutsAlignment = () => {
   // States
   const [isPasswordShown, setIsPasswordShown] = useState(false)
-  const [password, setPassword] = useState('')
-  const [userCin, setUserCin] = useState('')
-
-  const { authData } = UseAuthStore()
-  const { login } = UseAuthStore()
-
-  useEffect(() => {
-    // ;(async () => {
-    //   const res = await fetch('http://192.168.60.148:3000/unite')
-
-    //   console.log('test the api :', res)
-    // })()
-
-    if (authData?.token) {
-      redirect('/home')
-    }
-  }, [authData])
-
-  // Vars
-  const darkImg = '/images/pages/auth-mask-dark.png'
-  const lightImg = '/images/pages/auth-mask-light.png'
-  const darkIllustration = '/images/illustrations/auth/v2-login-dark.png'
-  const lightIllustration = '/images/illustrations/auth/v2-login-light.png'
-  const borderedDarkIllustration = '/images/illustrations/auth/v2-login-dark-border.png'
-  const borderedLightIllustration = '/images/illustrations/auth/v2-login-light-border.png'
-
-  // Hooks
-  const router = useRouter()
-  const { settings } = useSettings()
-  const theme = useTheme()
-  const hidden = useMediaQuery(theme.breakpoints.down('md'))
-  const authBackground = useImageVariant(mode, lightImg, darkImg)
-
-  const characterIllustration = useImageVariant(
-    mode,
-    lightIllustration,
-    darkIllustration,
-    borderedLightIllustration,
-    borderedDarkIllustration
-  )
 
   const handleClickShowPassword = () => setIsPasswordShown(show => !show)
 
+  const [cni, setCni] = useState('')
+  const [pass, setPass] = useState('')
+  const [error, setError] = useState('')
+
+  const onsubmit = async () => {
+    if (cni && pass) {
+      console.log(cni, pass)
+    } else {
+      setError('Veuillez entrer votre CNI et votre mot de passe.')
+    }
+  }
+
   return (
-    <div className='flex bs-full justify-center'>
-      <div
-        className={classnames(
-          'flex bs-full items-center justify-center flex-1 min-bs-[100dvh] relative p-6 max-md:hidden',
-          {
-            'border-ie': settings.skin === 'bordered'
-          }
-        )}
-      >
-        <LoginIllustration src={characterIllustration} alt='character-illustration' />
-        {!hidden && (
-          <MaskImg
-            alt='mask'
-            src={authBackground}
-            className={classnames({ 'scale-x-[-1]': theme.direction === 'rtl' })}
-          />
-        )}
-      </div>
-      <div className='flex justify-center items-center bs-full bg-backgroundPaper !min-is-full p-6 md:!min-is-[unset] md:p-12 md:is-[480px]'>
-        <div className='absolute block-start-5 sm:block-start-[33px] inline-start-6 sm:inline-start-[38px]'>
-          <Logo />
-        </div>
-        <div className='flex flex-col gap-6 is-full sm:is-auto md:is-full sm:max-is-[400px] md:max-is-[unset] mbs-11 sm:mbs-14 md:mbs-0'>
-          <div className='flex flex-col gap-1'>
-            <Typography variant='h4'>{`Welcome to ${themeConfig.templateName}! 👋🏻`}</Typography>
-            <Typography>Please sign-in to your account and start the adventure</Typography>
-          </div>
-          <form
-            noValidate
-            autoComplete='off'
-            onSubmit={e => {
-              e.preventDefault()
-              login(password, userCin)
-              // router.push('/')
-            }}
-            className='flex flex-col gap-5'
-          >
-            <CustomTextField
-              onChange={e => setUserCin(e.target.value)}
-              autoFocus
-              fullWidth
-              label='Email or Username'
-              placeholder='Enter your email or username'
-            />
-            <CustomTextField
-              fullWidth
-              label='Password'
-              placeholder='············'
-              id='outlined-adornment-password'
-              type={isPasswordShown ? 'text' : 'password'}
-              onChange={e => setPassword(e.target.value)}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position='end'>
-                    <IconButton edge='end' onClick={handleClickShowPassword} onMouseDown={e => e.preventDefault()}>
-                      <i className={isPasswordShown ? 'tabler-eye-off' : 'tabler-eye'} />
-                    </IconButton>
-                  </InputAdornment>
-                )
-              }}
-            />
-            <div className='flex justify-between items-center gap-x-3 gap-y-1 flex-wrap'>
-              <FormControlLabel control={<Checkbox />} label='Remember me' />
-              <Typography className='text-end' color='primary' component={Link}>
-                Forgot password?
-              </Typography>
-            </div>
-            <Button fullWidth variant='contained' type='submit'>
-              Login
-            </Button>
-            <div className='flex justify-center items-center flex-wrap gap-2'>
-              <Typography>New on our platform?</Typography>
-              <Typography component={Link} color='primary'>
-                Create an account
-              </Typography>
-            </div>
-            <Divider className='gap-2 text-textPrimary'>or</Divider>
-            <div className='flex justify-center items-center gap-1.5'>
-              <IconButton className='text-facebook' size='small'>
-                <i className='tabler-brand-facebook-filled' />
-              </IconButton>
-              <IconButton className='text-twitter' size='small'>
-                <i className='tabler-brand-twitter-filled' />
-              </IconButton>
-              <IconButton className='text-textPrimary' size='small'>
-                <i className='tabler-brand-github-filled' />
-              </IconButton>
-              <IconButton className='text-error' size='small'>
-                <i className='tabler-brand-google-filled' />
-              </IconButton>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
+    <Card className=''>
+      <div className='absolute block-start-5 sm:block-start-[33px] inline-start-6 sm:inline-start-[38px]'>
+        <Logo />
+      </div>{' '}
+      <CardContent style={{ height: '100vh' }} className='flex flex-col items-center justify-center bs-[500px]'>
+        <form onSubmit={e => e.preventDefault()} className='p-12 max-is-[400px] border rounded'>
+          <Grid container spacing={6}>
+            <Grid item xs={12}>
+              <Typography variant='h5'>S&apos;inscrire</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <CustomTextField
+                value={cni}
+                onChange={e => setCni(e.target.value)}
+                fullWidth
+                label='CNI'
+                placeholder=' '
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <CustomTextField
+                fullWidth
+                label='Mot de passe'
+                placeholder='············'
+                id='form-layout-alignment-password'
+                type={isPasswordShown ? 'text' : 'password'}
+                value={pass}
+                onChange={e => setPass(e.target.value)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position='end'>
+                      <IconButton
+                        edge='end'
+                        onClick={handleClickShowPassword}
+                        onMouseDown={e => e.preventDefault()}
+                        aria-label='toggle password visibility'
+                      >
+                        <i className={isPasswordShown ? 'tabler-eye-off' : 'tabler-eye'} />
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }}
+              />
+              {error && (
+                <FormHelperText className='mt-4' error>
+                  {error}
+                </FormHelperText>
+              )}
+            </Grid>
+            <Grid item xs={12} className='pbs-2'>
+              <FormControlLabel control={<Checkbox />} label="Ne m'oublie pas" />
+            </Grid>
+            <Grid item xs={12} className='pbs-2'>
+              <Button onClick={onsubmit} variant='contained' type='submit' fullWidth>
+                Se connecter
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </CardContent>
+    </Card>
   )
 }
 
-export default LoginV2
+export default FormLayoutsAlignment
