741aa8c3a762ecf4d62976b8a10aa3ff8e2d5b1f
Abdelaziz kazoum <abdukazoum@gmail.com>
2024-09-09 16:32:52 +0200

    add search by user and direction

--- frontend/src/components/courriers/list/Index.tsx
+++ frontend/src/components/courriers/list/Index.tsx
@@ -5,17 +5,31 @@ import { useEffect } from 'react'
 
 import { CircularProgress, Grid } from '@mui/material'
 
+import { useSession } from 'next-auth/react'
+
 import { useCourrierStore } from '@/store/courrier.store'
 import { CourriersList } from './CourriersList'
+import { useDirectionStore } from '@/store/direction.store'
+
+import { UseUtilisateurStore } from '@/store/utilisateur.store'
 
 const Index = () => {
   const { fetchCourriers, courriers } = useCourrierStore()
+  const { fetchDirections } = useDirectionStore()
+  const { fetchUsers } = UseUtilisateurStore()
+
+  //auth
+  const { data: session } = useSession()
 
   useEffect(() => {
     ;(async () => {
+      await fetchDirections()
       await fetchCourriers()
+      if (session?.user?.role === 'Administrateur') {
+        await fetchUsers()
+      }
     })()
-  }, [fetchCourriers])
+  }, [fetchCourriers, fetchDirections, fetchUsers, session?.user?.role])
 
   return (
     <div>
--- frontend/src/components/courriers/list/tableFilters.tsx
+++ frontend/src/components/courriers/list/tableFilters.tsx
@@ -17,6 +17,9 @@ import { MenuItem } from '@mui/material'
 import type { UtilisateurType } from '@/types/userTypes'
 
 import type { CourrierType } from '@/types/courrierTypes'
+import { useDirectionStore } from '@/store/direction.store'
+import { DirectionType } from '@/types/directionType'
+import { UseUtilisateurStore } from '@/store/utilisateur.store'
 
 const TableFilters = ({
   setData,
@@ -27,26 +30,58 @@ const TableFilters = ({
 }) => {
   // States
   const [status, setStatus] = useState<CourrierType['status']>('')
+  const [direction, setDirection] = useState<DirectionType['nom_direction']>('')
+
   const [type, setType] = useState<CourrierType['type']>('')
   const [utilisateur, setUtilisateur] = useState<UtilisateurType['cin']>()
 
-  // const [direction, setDirection] = useState<UtilisateurType['direction']>()
+  // Store
+  const { directions } = useDirectionStore()
+  const { users } = UseUtilisateurStore()
 
   useEffect(() => {
     const filteredData = tableData?.filter(courrier => {
       if (status && courrier.status !== status) return false
       if (type && courrier.type !== type) return false
       if (utilisateur && courrier.utilisateur.cin !== utilisateur) return false
+      if (direction && courrier.utilisateur.direction?.nom_direction !== direction) return false
 
       return true
     })
 
     setData(filteredData || [])
-  }, [status, type, utilisateur, tableData, setData])
+  }, [status, type, utilisateur, tableData, setData, direction])
 
   return (
     <CardContent>
       <Grid container spacing={6}>
+        {
+          <>
+            <Grid item xs={12} sm={4}>
+              <CustomTextField
+                select
+                fullWidth
+                defaultValue=''
+                label='Direction'
+                id='custom-select'
+                value={direction}
+                onChange={e => setDirection(e.target.value)}
+                SelectProps={{ displayEmpty: true }}
+              >
+                <MenuItem value=''>
+                  <em>Aucun</em>
+                </MenuItem>
+                {directions?.map((item: DirectionType) => (
+                  <MenuItem key={item.id} value={item.nom_direction}>
+                    {' '}
+                    {item.nom_direction}{' '}
+                  </MenuItem>
+                ))}
+              </CustomTextField>
+            </Grid>
+          </>
+        }
+
         <Grid item xs={12} sm={4}>
           <CustomTextField
             select
@@ -83,7 +118,7 @@ const TableFilters = ({
             <MenuItem value='SORTANT'>Sortant</MenuItem>
           </CustomTextField>
         </Grid>
-        <Grid item xs={12} sm={4}>
+        {/* <Grid item xs={12} sm={4}>
           <CustomTextField
             fullWidth
             label='Utilisateur'
@@ -92,6 +127,27 @@ const TableFilters = ({
             onChange={e => setUtilisateur(e.target.value)}
             SelectProps={{ displayEmpty: true }}
           ></CustomTextField>
+        </Grid> */}
+        <Grid item xs={12} sm={4}>
+          <CustomTextField
+            select
+            fullWidth
+            defaultValue=''
+            label='Utilisateur'
+            id='custom-select'
+            value={utilisateur}
+            onChange={e => setUtilisateur(e.target.value)}
+            SelectProps={{ displayEmpty: true }}
+          >
+            <MenuItem value=''>
+              <em>Aucun</em>
+            </MenuItem>
+            {users?.map((item: UtilisateurType) => (
+              <MenuItem key={item.id} value={item.cin}>
+                {` ${item.nom} ${item.prenom}   ${item.compte?.pseudo && ` (${item.compte?.pseudo || ''}) `}  `}
+              </MenuItem>
+            ))}
+          </CustomTextField>
         </Grid>
       </Grid>
     </CardContent>
